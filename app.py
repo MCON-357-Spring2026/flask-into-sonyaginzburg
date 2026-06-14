3024c0840e6b3dc692fe7cb8fe7938691614218e
Sonya <sginzbur@student.touro.edu>
2026-06-14 00:01:44 -0400

    hw in progress

--- app.py
+++ app.py
@@ -2,6 +2,21 @@ from flask import Flask, request, current_app, jsonify
 
 app = Flask(__name__)
 
+# Homework - adding Hooks
+@app.before_request
+def log_request():
+    print(f"[before_request] {request.method} {request.path}")
+
+@app.after_request
+def add_header(response):
+    response.headers["X-Custom-Header"] = "FlaskRocks"
+    return response # has to return the response object
+
+@app.teardown_request
+def log_exception(exception):
+    if exception:
+        print(f"[teardown_request] Exception: {exception}")
+
 # 1 welcome
 @app.route("/", methods=["GET"])
 def welcome():
@@ -11,23 +26,24 @@ def welcome():
 @app.route("/about", methods=["GET"])
 def about():
     return jsonify({
-        "name": "Your Name",
+        "name": "Sonya G",
         "course": "MCON-504 - Backend Development",
         "semester": "Spring 2025"
     })
 
 # 3 Greeting
-@app.route("/greet/Leah", methods=["GET"])
+@app.route("/greet/<name>", methods=["GET"])
 def greet():
-    return "<p>Hello, Leah! Welcome to Flask.</p>"
+    return "<p>Hello, {name}! Welcome to Flask.</p>"
 
 # 4 calculate
+# made some changes for exception handling
 
 @app.route("/calculate", methods=["GET"])
 def calculate():
     #get parameteres
-    num1 = request.args.get("num1")
-    num2 = request.args.get("num2")
+    num1 = float(request.args.get("num1", 0))
+    num2 = float(request.args.get("num2", 0))
     operation = request.args.get("operation")
 
     if operation == "add":
@@ -37,7 +53,7 @@ def calculate():
     elif operation == "multiply":
         result = num1 * num2
     elif operation == "divide":
-        result = num1/ num2;
+        result = num1/ num2
     else:
         return jsonify({"error": "Invalid operation"}), 400
 
@@ -52,7 +68,7 @@ def calculate():
 def echo():
     data = request.get_json()
     data["echoed"] = True
-    return jsonify({data})
+    return jsonify(data)
 
 
 
@@ -62,4 +78,4 @@ def status(code):
     return f"This is a {code} status response", code
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=5000)
